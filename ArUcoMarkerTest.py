375b0a8eeb0c093163ba125a476d5c48be260798
Leo <leo380687@gmail.com>
2025-10-08 15:58:44 +0800

    Update multiple cards detection by specific scanning direction

--- ArUcoMarkerTest.py
+++ ArUcoMarkerTest.py
@@ -25,32 +25,40 @@ def ArUcoMarkerGeneration():
 
 
 # A function to read image file and detect if marker exists
-def readArUcoMarker():
+def readArUcoMarker(scanningDirection):
     #load a marker's image
     # img = cv2.imread("MilkCo Process Cards/TransportDieselTruck.png",0)
     # img = cv2.imread("TransportDieselTruckTakenByiPhone(4284x5712).png",0)
-    img = cv2.imread("TransportDieselTruckTakenByiPhone(1280x720).png",0)
+    img = cv2.imread("MultipleCardsH.png",0)
 
     # Define a Aruco detector by given dictionary
     parameters = aruco.DetectorParameters()
     detector = aruco.ArucoDetector(aruco_dict,parameters)
     # Detect an image with the marker
     corners, ids, rejected = detector.detectMarkers(img)
-    userresponse = input("Which direction would you want to search? (Vertical, Horizontal, in terms of card ID): ")
     markers = []
+    sorted_markers = []
     if ids is not None:
+        print(ids.flatten())
         for i, marker_id in enumerate(ids.flatten()):
             marker_corners = corners[i][0] # return a list of x y coordinates of a marker
             corners_mean_of_x = marker_corners[:,0].mean() # calculating mean of x coordinate of four corners
             corners_mean_of_y = marker_corners[:,1].mean() # calculating mean of x coordinate of four corners
             markers.append({"id":marker_id, "XMean":corners_mean_of_x, "YMean": corners_mean_of_y})
-        sorted_markers = markers.sort(key=lambda marker: marker.get("XMean"))
-        if(userresponse == "Vertical"):
-            pass
-        print()
-        print("Detected IDs:", ids.flatten())
+        print(markers)
+        if(scanningDirection == "Vertical"):
+            sorted_markers = sorted(markers, key=lambda marker: marker["YMean"])
+        elif(scanningDirection == "Horizontal"):
+            sorted_markers = sorted(markers, key=lambda marker: marker["XMean"])
+        elif(scanningDirection == "in terms of card ID"):
+            sorted_markers = markers.sort(key= lambda marker: marker.get("id"))
+        print(sorted_markers)
+        sorted_ids = [marker["id"] for marker in sorted_markers]
+        print(*sorted_ids)
     else:
         print("No markers detected")
 # ArUcoMarkerGeneration()
-readArUcoMarker()
+userResponse = input("Which direction would you want to search? (Vertical, Horizontal, in terms of card ID): ")
+readArUcoMarker(userResponse)
+
 
